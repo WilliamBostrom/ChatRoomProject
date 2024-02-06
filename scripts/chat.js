f3b1a84aa93ee698f895500e4b52f76c2bbf5028
WilliamBostrom <133894488+WilliamBostrom@users.noreply.github.com>
2024-02-06 11:21:35 +0100

    init commit

---
+++ scripts/chat.js
@@ -0,0 +1,56 @@
+// adding new chats
+
+// setting up real-time listners to get new chats
+
+// updating the username
+
+// updating the room
+
+class Chatroom {
+  constructor(room, username) {
+    this.room = room;
+    this.username = username;
+    this.chats = db.collection("chats");
+    this.unsub;
+  }
+
+  async addChat(message) {
+    // format a chatobj
+    const now = new Date();
+    const chat = {
+      message,
+      username: this.username,
+      room: this.room,
+      created_at: firebase.firestore.Timestamp.fromDate(now),
+    };
+    // save chat doc
+
+    const response = await this.chats.add(chat);
+    return response;
+  }
+
+  getChats(callback) {
+    this.unsub = this.chats
+      .where("room", "==", this.room)
+      .orderBy("created_at")
+      .onSnapshot((snapshot) => {
+        snapshot.docChanges().forEach((change) => {
+          if (change.type === "added") {
+            // update ui
+            callback(change.doc.data());
+          }
+        });
+      });
+  }
+  updateName(username) {
+    this.username = username;
+    localStorage.setItem("username", username);
+  }
+  updateRoom(room) {
+    this.room = room;
+    console.log("room updated");
+    if (this.unsub) {
+      this.unsub();
+    }
+  }
+}
