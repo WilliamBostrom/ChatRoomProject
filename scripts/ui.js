5e743dc0a9d9d5d1c9e4e63b386c6c14fee32f8f
WilliamBostrom <133894488+WilliamBostrom@users.noreply.github.com>
2024-02-09 01:28:29 +0100

    dateFns remove

--- scripts/ui.js
+++ scripts/ui.js
@@ -8,16 +8,17 @@ class ChatUI {
   }
 
   render(data) {
-    const when = dateFns.distanceInWordsToNow(data.created_at.toDate(), {
-      addSuffix: true,
-    });
+    // const when = dateFns.distanceInWordsToNow(data.created_at.toDate(), {
+    //   addSuffix: true,
+    // });
     const html = `
     <li class="list-group-item"> 
     <span class="username">${data.username}</span>
     <span class="message">${data.message}</span>
-    <div class="time">${when}</div>
+ 
     </li>`;
 
     this.list.innerHTML += html;
   }
 }
+// <div class="time">${when}</div>
